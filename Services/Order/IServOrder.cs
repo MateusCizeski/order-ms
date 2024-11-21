412a4293d7375df6a44fd983b573966715f65bb6
Mateus Cizeski <mateuscizeski@gmail.com>
2024-11-21 19:42:30 -0300

    nova estrtura de services.

---
+++ Services/Order/IServOrder.cs
@@ -0,0 +1,13 @@
+﻿using domain;
+
+namespace services
+{
+    public interface IServOrder
+    {
+        Order InsertOrder(Order order);
+        Order GetOrderById(int id);
+        Order EditOrder(Order order);
+        List<Order> ListOrders();
+        void DeleteOrder(int id);
+    }
+}
